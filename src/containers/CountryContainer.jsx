9028a2351e14eac8b0b2d79565c4397c7d636cd3
Leila Peltier <l_peltier@outlook.com>
2024-03-25 14:25:25 +0000

    have been able to load the data

--- src/containers/CountryContainer.jsx
+++ src/containers/CountryContainer.jsx
@@ -1,14 +1,31 @@
+import { useState, useEffect } from "react";
+import CountryList from "../components/CountryList";
+
 const CountryContainer = () => {
 
+    const [countries, setCountries] = useState(null);
+
+     //fetching data 
+     const loadCountries = async() =>{
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const jsonData = await response.json();
+        setCountries(jsonData);
+    }
+
+    //using useEffect to get the data to load when the app runs 
+    useEffect(() => {
+        loadCountries();
+    }, []);
+
+    useEffect(() =>{
+        console.log(countries);
+    },[countries]);
 
     return (
-
         <>
-        
+            {JSON.stringify(countries)}
         </>
-
-
-    );
+    ); 
 };
 
 export default CountryContainer;
